d23383f96eafb83caa450dd813d278b4170d6990
thiago <Thiago Benevide>
2022-02-18 11:10:48 -0300

    Animaçao criada no site

--- index-Script.js
+++ index-Script.js
@@ -11,3 +11,53 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show
 })
+
+
+
+/*Code Debaunce*/ 
+
+const debounce = function(func, wait, immediate){
+    let timeout
+    return function(...args){
+        const context = this
+        const later = function(){
+            timeout = null
+            if (!immediate) func.apply(context, args)
+        }
+        const callNow = immediate && !timeout
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) func.apply(context, args)
+    }
+}
+
+//Transition Projects
+
+const target = document.querySelectorAll("[data-anime]")
+const animationClass = "animate"
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight*3)/4)
+    target.forEach(function(element){
+
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass)
+        }else{
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll()
+
+/*Otimizations*/
+
+if(target.length){
+    window.addEventListener("scroll", debounce(function(){
+        animeScroll()
+        console.log("asdasd")
+    }, 100))
+}
+
+
+
